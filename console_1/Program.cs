366af241b23ff4a7d10fe5a3eb9026c4d2a2d5e6
metetik <metetik@outlook.com>
2021-10-23 12:46:54 +0300

    first console application

---
+++ console_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace console_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminizi Girin : ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Selam, " + name);
+        }
+    }
+}
